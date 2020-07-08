5a320a22005e50443e66488809bec788e3aefdab
Guilherme <guirlviana@gmail.com>
2020-07-08 15:28:41 -0300

    Adicionado sorteio de senhas

--- guessgame.rb
+++ guessgame.rb
@@ -9,25 +9,24 @@
   puts "------------------------------"
 
 
+secrets_world = ['Ruby', 'Human', 'Apple', 'Car', 'House', 'Code']
+worldpass = secrets_world.shuffle
+secret_world = worldpass[1]
 
-secret_world = "Ruby"
 guess = ""
 guess_count = 0
 guess_limit = 5
 out_of_guesses = false
 
 while guess != secret_world and !out_of_guesses 
-if guess_count < guess_limit
-  puts "Enter guess: "
-  guess = gets.chomp()
-  guess_count += 1
-  guess_limit = 5
-else
-  out_of_guesses = true
-end
-
-
-
+  if guess_count < guess_limit
+    puts "Enter guess: "
+    guess = gets.chomp()
+    guess_count += 1
+    guess_limit = 5
+  else
+    out_of_guesses = true
+  end
 end
 
 if out_of_guesses
